c1061b34c50946148f07dc8939c05cac48c23fed
Hicham Bakri <hicham.bakri76@gmail.com>
2021-09-20 14:27:33 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import UploadImages from './screens/upload-images';
 import Login from './screens/list-images';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
